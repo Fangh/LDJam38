c3b97cf3d00b11e0d80c689b8c4ba5ea7f7131cb
Unknown <toto.klein@laposte.net>
2017-04-22 13:18:57 +0200

    can now click on a button + integration des graph de yann

---
+++ Assets/Scripts/Bonus.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Bonus : MonoBehaviour 
+{
+	public void LaunchSkill()
+	{
+		Color randomHSV = Random.ColorHSV();
+		GetComponent<Image>().color = randomHSV;
+	}
+}
