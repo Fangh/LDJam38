f8a6faa0a516e823bdebab6d31ec55be6293f2aa
Unknown <alison.derolez@gmail.com>
2017-04-23 00:57:21 +0200

    Maj Scripts (ListeName)

--- Assets/Scripts/CheatCode.cs
+++ Assets/Scripts/CheatCode.cs
@@ -4,7 +4,13 @@ using System.Collections;
 public class CheatCode : MonoBehaviour 
 {
 	public float timescale = 1;
-	
+
+	void Start () 
+	{
+		string name = "lol";
+		Debug.Log (name);
+	}
+
 	// Update is called once per frame
 	void Update () 
 	{
