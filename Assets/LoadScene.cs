b73e87fd90d8f0cb2b0f0a0defd62502f176aae5
Unknown <alison.derolez@gmail.com>
2017-04-23 17:23:17 +0200

    Création MainMenu

---
+++ Assets/LoadScene.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+
+	public void Loadlevel()
+	{
+		SceneManager.LoadScene("Level1");
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
